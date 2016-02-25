a6e78d9e326cfe5cbd94111d19bb7fba7a5bf9eb
Deirdre Storck <deirdre.storck@gmail.com>
2016-02-25 14:29:08 -0800

    added wordBank class and get_word method to generate answer

--- word_guess.rb
+++ word_guess.rb
@@ -7,3 +7,21 @@ class WordGuess
   end
 
 end
+
+#include bank of words, get_word method that will choose a word and puts the word in stars, remove word from bank
+class WordBank
+
+  def initialize
+    @words = %w(cat dog hello there pie mug hat fox door bear frog mice)
+  end
+
+  def get_word
+    @answer = @words.sample
+    @words.delete(@answer)
+    length_of_word = @answer.length
+    puts " * " * length_of_word
+    #we will need to make this word into an array so that we can replace stars with correct letter guesses
+
+  end
+
+end

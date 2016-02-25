22eaddf9367f791d8069eb8838fd123439b53dd2
Deirdre Storck <deirdre.storck@gmail.com>
2016-02-25 15:57:05 -0800

    set up instance variables in GuessingGame initialize method

--- word_guess.rb
+++ word_guess.rb
@@ -1,13 +1,21 @@
 class GuessingGame
+  attr_accessor :wordbank_answer, :answer_validation_array
+  def initialize
+    @wordbank_answer = WordBank.new
+    @answer_validation_array = @wordbank_answer.get_word
+  end
 
 #print guessed_letters when not nil to show user what they have guessed
 #compare guessed_letters to answer array to print stars
 #compare WordGuess.letter_guess to WordBank.answer_array to see if correct or incorrect
+  def print_answer_blanks
+
+  end
 
 end
 
 class WordGuess
-
+  attr_accessor :guessed_letters, :guess
   def initialize
     @guessed_letters = []
   end
@@ -26,6 +34,7 @@ end
 
 #include bank of words, get_word method that will choose a word and puts the word in stars, remove word from bank
 class WordBank
+  attr_accessor :answer_array, :answer
 
   def initialize
     @words = %w(cat dog hello there pie mug hat fox door bear frog mice)
@@ -36,6 +45,7 @@ class WordBank
     @words.delete(@answer)
 
     @answer_array = @answer.split(//)
+
 
     #we will need to make this word into an array so that we can replace stars with correct letter guesses
 
